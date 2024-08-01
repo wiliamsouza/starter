7809cbfc42064bcd30d2a1249c8b27af0573737c
Wiliam Souza <wiliamsouza83@gmail.com>
2024-07-31 23:35:57 -0300

    Golang config

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,18 +1,41 @@
 -- load defaults i.e lua_lsp
 require("nvchad.configs.lspconfig").defaults()
+local ih = require "inlay-hints"
 
 local lspconfig = require "lspconfig"
 
 -- EXAMPLE
-local servers = { "html", "cssls" }
+local servers = { "html", "cssls", "gopls" }
 local nvlsp = require "nvchad.configs.lspconfig"
 
 -- lsps with default config
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
-    on_attach = nvlsp.on_attach,
+    --on_attach = nvlsp.on_attach,
+    on_attach = function(c, b)
+      nvlsp.on_attach(c, b)
+      ih.on_attach(c, b)
+    end,
     on_init = nvlsp.on_init,
     capabilities = nvlsp.capabilities,
+    settings = {
+      gopls = {
+        completeUnimported = true,
+        usePlaceholders = true,
+        analyses = {
+          unusedparams = true,
+        },
+        hints = {
+          assignVariableTypes = true,
+          compositeLiteralFields = true,
+          compositeLiteralTypes = true,
+          constantValues = true,
+          functionTypeParameters = true,
+          parameterNames = true,
+          rangeVariableTypes = true,
+        },
+      },
+    },
   }
 end
 
